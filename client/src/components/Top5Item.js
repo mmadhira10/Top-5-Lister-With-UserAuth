37b7e02a216e5a4e193409287297a348e5daf8f2
Mihir Madhira <mihir.madhira@stonybrook.edu>
2021-11-09 21:23:28 +0000

    listCard changes

--- client/src/components/Top5Item.js
+++ client/src/components/Top5Item.js
@@ -15,6 +15,7 @@ function Top5Item(props) {
     const { store } = useContext(GlobalStoreContext);
     const [editActive, setEditActive] = useState(false);
     const [draggedTo, setDraggedTo] = useState(0);
+    const [ text, setText ] = useState("");
 
     function handleDragStart(event, targetId) {
         event.dataTransfer.setData("item", targetId);
@@ -48,6 +49,35 @@ function Top5Item(props) {
         store.addMoveItemTransaction(sourceId, targetId);
     }
 
+    function toggleEdit() {
+        let newActive = !editActive;
+        if (newActive) {
+            store.setIsItemEditActive();
+        }
+        setEditActive(newActive);
+    }
+
+    function handleToggleEdit(event) {
+        event.stopPropagation();
+        toggleEdit();
+    }
+
+    function handleKeyPress(event) {
+        if (event.code === "Enter") {
+            handleBlur();
+        }
+    }
+
+    function handleBlur(event)
+    {
+        store.addUpdateItemTransaction(index, text);
+        toggleEdit();
+    }
+
+    function handleUpdateText(event) {
+        setText(event.target.value );
+    }
+
     let { index } = props;
 
     let itemClass = "top5-item";
@@ -55,40 +85,63 @@ function Top5Item(props) {
         itemClass = "top5-item-dragged-to";
     }
 
+    let itemCard =             
+    <ListItem
+        id={'item-' + (index+1)}
+        key={props.key}
+        className={itemClass}
+        onDragStart={(event) => {
+            handleDragStart(event, (index+1))
+        }}
+        onDragOver={(event) => {
+            handleDragOver(event, (index+1))
+        }}
+        onDragEnter={(event) => {
+            handleDragEnter(event, (index+1))
+        }}
+        onDragLeave={(event) => {
+            handleDragLeave(event, (index+1))
+        }}
+        onDrop={(event) => {
+            handleDrop(event, (index+1))
+        }}
+        draggable="true"
+        sx={{ display: 'flex', p: 1 }}
+        style={{
+            fontSize: '48pt',
+            width: '100%'
+        }}
+    >
+    <Box sx={{ p: 1 }}>
+        <IconButton onClick={handleToggleEdit}aria-label='edit'>
+            <EditIcon style={{fontSize:'48pt'}}  />
+        </IconButton>
+    </Box>
+        <Box sx={{ p: 1, flexGrow: 1 }}>{props.text}</Box>
+    </ListItem>
+
+    if (editActive) {
+        itemCard =
+        <TextField
+            margin="normal"
+            required
+            fullWidth
+            id={"list-" + index + 1}
+            label="Top 5 List Name"
+            name="name"
+            autoComplete="Top 5 List Name"
+            className='list-card'
+            onKeyPress={handleKeyPress}
+            onChange={handleUpdateText}
+            defaultValue={props.text}
+            inputProps={{style: {fontSize: 48}}}
+            InputLabelProps={{style: {fontSize: 24}}}
+            autoFocus
+        />
+    }
+
     return (
-            <ListItem
-                id={'item-' + (index+1)}
-                key={props.key}
-                className={itemClass}
-                onDragStart={(event) => {
-                    handleDragStart(event, (index+1))
-                }}
-                onDragOver={(event) => {
-                    handleDragOver(event, (index+1))
-                }}
-                onDragEnter={(event) => {
-                    handleDragEnter(event, (index+1))
-                }}
-                onDragLeave={(event) => {
-                    handleDragLeave(event, (index+1))
-                }}
-                onDrop={(event) => {
-                    handleDrop(event, (index+1))
-                }}
-                draggable="true"
-                sx={{ display: 'flex', p: 1 }}
-                style={{
-                    fontSize: '48pt',
-                    width: '100%'
-                }}
-            >
-            <Box sx={{ p: 1 }}>
-                <IconButton aria-label='edit'>
-                    <EditIcon style={{fontSize:'48pt'}}  />
-                </IconButton>
-            </Box>
-                <Box sx={{ p: 1, flexGrow: 1 }}>{props.text}</Box>
-            </ListItem>
+        itemCard
     )
 }
 
